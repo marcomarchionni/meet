1dcf112ca333b8e72b917af7a0da9fadc228886e
Marco Marchionni <marco.marchionni@gmail.com>
2023-05-04 18:01:17 +0200

    ScatterChart and PieChart added

---
+++ src/EventsCityChart.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from 'react';
+import {
+  CartesianGrid,
+  ResponsiveContainer,
+  Scatter,
+  ScatterChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
+import { Schema$Event } from './interfaces/google-interfaces';
+
+interface ChartProps {
+  events: Schema$Event[];
+  locations: string[];
+}
+
+const CityScatterChart = ({ events, locations }: ChartProps) => {
+  const [chartData, setChartData] = useState<Object[]>([]);
+
+  useEffect(() => {
+    const data = locations.map((location) => {
+      const number = events.filter(
+        (event) => event.location === location,
+      ).length;
+      const city = location.split(', ').shift();
+      return { city, number };
+    });
+    setChartData(data);
+  }, [events, locations]);
+
+  return (
+    <ResponsiveContainer height={400} className="scatter-chart">
+      <ScatterChart
+        margin={{
+          top: 20,
+          right: 0,
+          bottom: 10,
+          left: 0,
+        }}>
+        <CartesianGrid stroke="#cccccc" strokeDasharray="3 3" />
+        <XAxis
+          dataKey="city"
+          type="category"
+          name="city"
+          stroke="#cccccc"
+          tickLine={false}
+          tick={{ fill: '#757575' }}
+        />
+        <YAxis
+          dataKey="number"
+          type="number"
+          allowDecimals={false}
+          name="number of events"
+          stroke="#cccccc"
+          tickLine={false}
+          tick={{ fill: '#757575' }}
+        />
+        <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+        <Scatter name="Events" data={chartData} fill="#96B0AF" />
+      </ScatterChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default CityScatterChart;
